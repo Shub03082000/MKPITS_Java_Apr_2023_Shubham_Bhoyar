89f051bc928e706e0ecb6db10c796312e39f75d5
Shub03082000 <113837522+Shub03082000@users.noreply.github.com>
2023-06-05 16:03:12 +0530

    Update MainMultiply.java

--- JAVA/Operator_in_java/MainMultiply.java
+++ JAVA/Operator_in_java/MainMultiply.java
@@ -28,22 +28,22 @@ class MainMultiply{
 		
 		/*Addition Operation using Integer datatype*/
 			MultiplcationArithmaticOperator multiply1 = new MultiplcationArithmaticOperator();
-			System.out.println("Addition of two Integer number is : " + (multiply1.multiplyInteger(40,80)));
+			System.out.println("Multiplication of two Integer number is : " + (multiply1.multiplyInteger(40,80)));
 		
 		/*Addition Operation using Float datatype*/
 			MultiplcationArithmaticOperator multiply2 = new MultiplcationArithmaticOperator();
-			System.out.println("Addition of two float number is : " + (multiply2.multiplyFloat(40f,20f)));
+			System.out.println("Multiplication of two float number is : " + (multiply2.multiplyFloat(40f,20f)));
 		
 		/*Addition Operation using Double datatype*/
 			MultiplcationArithmaticOperator multiply3 = new MultiplcationArithmaticOperator();
-			System.out.println("Addition of two Double number is : " + (multiply3.multiplyDouble(25.75,16.86)));
+			System.out.println("Multiplication of two Double number is : " + (multiply3.multiplyDouble(25.75,16.86)));
 	
 		/*Addition Operation using Long datatype*/
 			MultiplcationArithmaticOperator multiply4 = new MultiplcationArithmaticOperator();
-			System.out.println("Addition of two Long number is : " + (multiply4.multiplyLong(700,100)));
+			System.out.println("Multiplication of two Long number is : " + (multiply4.multiplyLong(700,100)));
 			
 		/*Addition Operation using Short datatype*/
 			//MultiplcationArithmaticOperator multiply5 = new MultiplcationArithmaticOperator();
-			//System.out.println("Addition of two Short number is : " + (multiply5.multiplyShort(7,2)));
+			//System.out.println("Multiplication of two Short number is : " + (multiply5.multiplyShort(7,2)));
 	}
 }
