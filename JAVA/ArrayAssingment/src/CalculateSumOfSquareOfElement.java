615e68bded93cc0303f0148d1c587a68c76cf1c2
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-07-03 18:01:15 +0530

    Sum of square of every 4th element using array including interface

---
+++ JAVA/ArrayAssingment/src/CalculateSumOfSquareOfElement.java
@@ -0,0 +1,5 @@
+//------------I have Create an interface with a name CalculateSumOfSquareOfElement
+public interface CalculateSumOfSquareOfElement {
+    //-----------This is method inside interface
+    public void SumOfSquareOfEvery4thElement();
+}
