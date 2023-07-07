8be45d2d5bc40f42d99c7bddcf2671292ef1ed14
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-07-07 06:39:23 +0530

    hollow star pattern

---
+++ JAVA/StarPatternAssingnemnt/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        HollowDiamondStarPattern hollowDiamondPattern = new HollowDiamondStarPattern();
+        hollowDiamondPattern.hollowDiamondStarPattern(0,0);
+    }
+}
