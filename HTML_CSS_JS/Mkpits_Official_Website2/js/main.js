4601b38fcf4dc0261273e9fc1e4053751e47d292
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-07-29 23:32:35 +0530

    modified file

--- HTML_CSS_JS/Mkpits_Official_Website2/js/main.js
+++ HTML_CSS_JS/Mkpits_Official_Website2/js/main.js
@@ -14,6 +14,7 @@
     
     // Initiate the wowjs
     new WOW().init();
+    
 
 
     // Fixed Navbar
@@ -47,6 +48,12 @@
         return false;
     });
 
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
 
     // Causes progress
     $('.causes-progress').waypoint(function () {
