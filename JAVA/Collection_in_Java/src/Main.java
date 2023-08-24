e8b59c9b80e251bba51bd15cdf1463d0716a60f2
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-08-24 22:17:44 +0530

    Method in ArrayList

---
+++ JAVA/Collection_in_Java/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+
+        list.add(60);//----> Integer(object)
+        list.add(56.90);//-----> Double(object)
+        list.add("shubham");//-----> String(object)
+
+
+    }
+}
