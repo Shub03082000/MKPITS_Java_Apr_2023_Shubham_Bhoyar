77fe0930fad4120ab096ce192af604b653ddfd0d
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-09-01 11:19:25 +0530

    updated code of CopyOnWriteArrayList

--- JAVA/CopyOnWriteArrayList_class/src/ClassCopyOnWriteArrayList.java
+++ JAVA/CopyOnWriteArrayList_class/src/ClassCopyOnWriteArrayList.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ClassCopyOnWriteArrayList {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ClassCopyOnWriteArrayList extends Thread{
+    static CopyOnWriteArrayList arrayList = new CopyOnWriteArrayList<String>();
+
+    @Override
+    public void run() {
+        arrayList.add("Shubham");
+        arrayList.add("Aniket");
+        arrayList.add("sanket");
+    }
+
+    public static void main(String[] args) {
+        arrayList.add("shivam");
+        arrayList.add("raj");
+
+        ClassCopyOnWriteArrayList classCopyOnWriteArrayList = new ClassCopyOnWriteArrayList();
+        classCopyOnWriteArrayList.start();
+
+        Iterator iterator = arrayList.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println(arrayList);
+    }
 }
