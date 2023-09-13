06f1882c4afbc591589ae27826b4cd2910e47c59
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-09-13 19:59:25 +0530

    Anonymous class

--- JAVA/AnonymousClassMain/src/AnonymousClassMain.java
+++ JAVA/AnonymousClassMain/src/AnonymousClassMain.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AnonymousClassMain {
+public class AnonymousClassMain {
+    static int number = 0;
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Pink");
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("yellow");
+            }
+        });
+    }
 }
+
