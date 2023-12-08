2fca8a8dcb2b2852ef088ba5d060cd90e10a3b13
Shubham_bhoyar <bhoyarshubham869@gmail.com>
2023-12-08 20:21:27 +0530

    spring boot project

---
+++ Spring_boot_project/springboot_DataJPA_CRUD/src/main/java/com/example/springboot_RestAPI_CRUD/rest/StudentController.java
@@ -0,0 +1,50 @@
+package com.example.springboot_RestAPI_CRUD.rest;
+
+import com.example.springboot_RestAPI_CRUD.entity.Student;
+import com.example.springboot_RestAPI_CRUD.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+    private StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return studentService.findAll();
+    }
+
+    @GetMapping("/students/{rollNo}")
+    public Student getStudent(@PathVariable Integer rollNo){
+       return studentService.findRecord(rollNo);
+    }
+
+    @PostMapping("/students")
+    public String insertStudent(@RequestBody Student student){
+        return studentService.insertRecord(student);
+    }
+
+//    @PutMapping("/students/{rollNo}")
+//    public Student updateStudent(@RequestBody Student student, @PathVariable Integer rollNo){
+//        return studentService.updateRecord(student);
+//    }
+
+    @PutMapping("/students")
+    public Student updateStudent(@RequestBody Student student){
+        return studentService.updateRecord(student);
+    }
+
+
+    @DeleteMapping("students/{rollNo}")
+    public String deleteStudent(@RequestBody @PathVariable Integer rollNo){
+        return studentService.deleteRecord(rollNo);
+    }
+}
---
+++ Spring_boot_project/springboot_DataJPA_CRUD/src/main/java/com/example/springboot_RestAPI_CRUD/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.example.springboot_RestAPI_CRUD.service;
+
+import com.example.springboot_RestAPI_CRUD.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> findAll();
+    Student findRecord(Integer rollNo);
+    String insertRecord(Student student);
+//    Student updateRecord(Student student, Integer rollNo);
+    Student updateRecord(Student student);
+
+    String deleteRecord(Integer rollNo);
+}
